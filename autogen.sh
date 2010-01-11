97fef10a15294fb2b294ed4814ee157683633a62
Andoni Morales Alastruey <amorales@flumotion.com>
2010-01-11 17:43:37 +0000

    * autoregen.sh (deleted): 	Delete autogenerated file 	* Makefile.am: 	* autogen.sh: 	* configure.ac: 	* intltool-extract.in: 	* intltool-merge.in: 	* intltool-update.in: 	* po/LINGUAS: 	* po/Makefile.in.in: 	* po/Makevars: 	* po/POTFILES.in: 	* po/es.po: 	* m4 (added): 	* intltool-extract.in (added): 	* intltool-update.in (added): 	* intltool-merge.in (added): 	* po (added): 	* po/LINGUAS (added): 	* po/es.po (added): 	* po/Makefile.in.in (added): 	* po/Makevars (added): 	* po/POTFILES.in (added): 	Added i18n support
    svn path=/flumotion-advanced/flumotion-fragmented-streaming/trunk/; revision=26472

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -x
 
-aclocal -I common || exit 1
-# libtoolize --force || exit 1
+libtoolize -c --force || exit 1
+autopoint --force || exit 1
+aclocal -I m4 -I common || exit 1
 # autoheader || exit 1
 autoconf || exit 1
-automake -a || exit 1
+automake -a -c -f || exit 1
 echo "./autogen.sh $@" > autoregen.sh
 chmod +x autoregen.sh
 ./configure $@
